abbf7bcca696fa3f6b82b3f4e1f6ed1a3e5f6997
송하규 <@DESKTOP-KQVK5QF>
2022-09-19 21:55:31 +0900

    초기 커밋.

---
+++ 3d/main.cpp
@@ -0,0 +1,75 @@
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+#define PI
+#define DEG2RAD
+#define RAD2DEG
+
+#pragma comment(lib, "OpenGL32")
+
+static void error_callback(int error, const char* description)
+{
+    fputs(description, stderr);
+}
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+
+int main(void)
+{
+    GLFWwindow* window;
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, key_callback);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        float ratio;
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        ratio = width / (float)height;
+
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glColor3f(0.0f, 0.0f, 1.0f);
+        double rad = 0.5;
+        double x[360], y[360];
+        glBegin(GL_LINE_STRIP);
+        for (int i = 0; i < 360; i++)
+        {
+            double angle;
+            angle = i * 3.141592 / 180;
+            x[i] = rad * cos(angle);
+            y[i] = rad * sin(angle);
+        }
+        for (int i = 0; i < 359; i++)
+        {
+            glVertex2f(x[i], y[i]);
+            glVertex2f(x[i + 1], y[i + 1]);
+        }
+        glEnd();
+        glFinish();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    exit(EXIT_SUCCESS);
+}
